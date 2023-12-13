055aea97da63c3eb1f2a329c032b0d2c72cd7fd9
Shawn <44221603+shaspitz@users.noreply.github.com>
2023-12-13 02:41:10 -0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,18 @@
 
 set -x
 
-JSON_RPC="http://sl-bootnode:8545"
+JSON_RPC="http://localhost:8545"
+
+# Check if contract already deployed
+DATA='{"jsonrpc":"2.0","method":"eth_getCode","params":["0x4e59b44847b379578588920ca78fbf26c0b4956c", "latest"],"id":1}'
+RESPONSE=$(curl -s -X POST --data "$DATA" -H "Content-Type: application/json" http://localhost:8545)
+CODE=$(echo $RESPONSE | jq -r '.result')
+if [ "$CODE" != "0x" ]; then
+    echo "Contract already deployed at 0x4e59b44847b379578588920ca78fbf26c0b4956c"
+    exit 0
+else
+    echo "No contract deployed at 0x4e59b44847b379578588920ca78fbf26c0b4956c. Deploying..."
+fi
 
 # start geth in a local container
 docker container run --rm -d --name deployment-proxy-geth -p 1234:8545 -e GETH_VERBOSITY=3 keydonix/geth-clique
@@ -10,14 +21,8 @@ docker container run --rm -d --name deployment-proxy-geth -p 1234:8545 -e GETH_V
 until curl --silent --fail $JSON_RPC -X 'POST' -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"net_version\", \"params\": []}"; do sleep 1; done
 
 # extract the variables we need from json output
-MY_ADDRESS="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" # Funded on genesis
-ONE_TIME_SIGNER_ADDRESS="0x$(cat output/deployment.json | jq --raw-output '.signerAddress')"
-GAS_COST="0x$(printf '%x' $(($(cat output/deployment.json | jq --raw-output '.gasPrice') * $(cat output/deployment.json | jq --raw-output '.gasLimit'))))"
 TRANSACTION="0x$(cat output/deployment.json | jq --raw-output '.transaction')"
 DEPLOYER_ADDRESS="0x$(cat output/deployment.json | jq --raw-output '.address')"
-
-# send gas money to signer
-curl $JSON_RPC -X 'POST' -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"eth_sendTransaction\", \"params\": [{\"from\":\"$MY_ADDRESS\",\"to\":\"$ONE_TIME_SIGNER_ADDRESS\",\"value\":\"$GAS_COST\"}]}"
 
 # deploy the deployer contract
 curl $JSON_RPC -X 'POST' -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"eth_sendRawTransaction\", \"params\": [\"$TRANSACTION\"]}"
