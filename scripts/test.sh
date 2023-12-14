405dc92449e2185554eeaa743cf1973d79f3f63b
Shawn <44221603+shaspitz@users.noreply.github.com>
2023-12-13 21:22:07 -0800

    no more ephemeral geth

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,16 @@
 
 set -x
 
-JSON_RPC="http://localhost:8545"
+if [ -z "$1" ]; then
+    echo "Usage: $0 <JSON_RPC_URL>"
+    exit 1
+fi
+JSON_RPC="$1"
+
+if ! [ -x "$(command -v curl)" ]; then
+    echo "Curl must be installed to deploy the create2 proxy" >&2
+    exit 1
+fi
 
 # Check if contract already deployed
 DATA='{"jsonrpc":"2.0","method":"eth_getCode","params":["0x4e59b44847b379578588920ca78fbf26c0b4956c", "latest"],"id":1}'
@@ -15,17 +24,25 @@ else
     echo "No contract deployed at 0x4e59b44847b379578588920ca78fbf26c0b4956c. Deploying..."
 fi
 
-# start geth in a local container
-docker container run --rm -d --name deployment-proxy-geth -p 1234:8545 -e GETH_VERBOSITY=3 keydonix/geth-clique
-# wait for geth to become responsive
-until curl --silent --fail $JSON_RPC -X 'POST' -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"net_version\", \"params\": []}"; do sleep 1; done
+# Check deployment signer has balance of 10000000000000000 wei allocated from genesis
+RESPONSE=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_getBalance","params":["0x3fab184622dc19b6109349b94811493bf2a45362", "latest"],"id":1}' -H "Content-Type: application/json" $JSON_RPC)
+if [ $(echo $RESPONSE | jq -r '.result') != "0x2386f26fc10000" ]; then
+    echo "Deployment signer (0x3fab184622dc19b6109349b94811493bf2a45362) must have balance of 10000000000000000 wei"
+    exit 1
+fi
 
-# extract the variables we need from json output
-TRANSACTION="0x$(cat output/deployment.json | jq --raw-output '.transaction')"
-DEPLOYER_ADDRESS="0x$(cat output/deployment.json | jq --raw-output '.address')"
+# Set presigned transaction 
+TRANSACTION="0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222"
 
-# deploy the deployer contract
+# deploy contract 
 curl $JSON_RPC -X 'POST' -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"eth_sendRawTransaction\", \"params\": [\"$TRANSACTION\"]}"
 
-# shutdown Parity
-docker container stop deployment-proxy-geth
+sleep 5
+
+# For prod we'll have to set gas params s.t. no ether is leftover here. For now we warn
+RESPONSE=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_getBalance","params":["0x3fab184622dc19b6109349b94811493bf2a45362", "latest"],"id":1}' -H "Content-Type: application/json" $JSON_RPC)
+if [ $(echo $RESPONSE | jq -r '.result') != "0x0" ]; then
+    echo "WARNING: Deployment signer (0x3fab184622dc19b6109349b94811493bf2a45362) has leftover balance of $(echo $RESPONSE | jq -r '.result') wei"
+fi
+
+
